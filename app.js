24e4ddaaaba9f40289b0cb892ba7ef92952ddd32
feindreh <119032218+feindreh@users.noreply.github.com>
2023-04-18 13:21:21 +0200

    add router

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const logger = require('morgan');
 
 const mongoose = require('mongoose');
 const indexRouter = require('./routes/index');
+const shopRouter = require('./routes/shop');
 
 // Set up mongoose connection
 const mongoDB = 'mongodb+srv://feindreh:2uqVgtMqmpSceLTg@cluster0.w63qaub.mongodb.net/shopContent?retryWrites=true&w=majority';
@@ -26,6 +27,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/shop', shopRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {

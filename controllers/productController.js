0538ee4850f7480335139ff34bcb6aba25749b7f
feindreh <119032218+feindreh@users.noreply.github.com>
2023-04-18 16:57:30 +0200

    add product_list

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,5 +1,8 @@
-exports.product_list = (req, res) => {
-  res.send('Implement product List');
+const Product = require('../models/product');
+
+exports.product_list = async (req, res) => {
+  const prods = await Product.find({});
+  res.render('product_list', { list: prods, title: 'Product List' });
 };
 
 exports.product_create_get = (req, res) => {

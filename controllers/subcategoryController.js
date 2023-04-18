1a6b4bbce6674d3c286ef59923e0aa8703a272f8
feindreh <119032218+feindreh@users.noreply.github.com>
2023-04-18 16:57:21 +0200

    add subcategory_list

--- controllers/subcategoryController.js
+++ controllers/subcategoryController.js
@@ -1,5 +1,8 @@
-exports.subcategory_list = (req, res) => {
-  res.send('Implement subcategory List');
+const SubCategory = require('../models/subcategory');
+
+exports.subcategory_list = async (req, res) => {
+  const subCats = await SubCategory.find({});
+  res.render('subcategory_list', { list: subCats, title: 'Sub-Category List' });
 };
 
 exports.subcategory_create_get = (req, res) => {

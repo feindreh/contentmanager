307070a9fa713c78d9a3ff2cd746df0028cdd7e1
feindreh <119032218+feindreh@users.noreply.github.com>
2023-04-18 15:10:30 +0200

    add category_list

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,13 +1,23 @@
+const { body, validationResult } = require('express-validator');
+const Category = require('../models/category');
+
+// display index page
 exports.index = (req, res) => {
   res.render('index', {
     title: 'Shop Home',
   });
 };
 
-exports.category_list = (req, res) => {
-  res.send('Implement Category List');
+// display list of categorys
+exports.category_list = async function (req, res, next) {
+  const cats = await Category.find({});
+  res.render('category_list', { list: cats, title: 'Category List' });
+};
+// display create form
+exports.category_create_get = (req, res) => {
+  res.render('category_form');
 };
 
-exports.category_create_get = (req, res) => {
-  res.send('Implement Category Create Get');
+exports.category_create_post = (req, res) => {
+  res.send('make post');
 };

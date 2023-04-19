e04e55609e913ddd039c06bb876e006e7f6cdcff
feindreh <119032218+feindreh@users.noreply.github.com>
2023-04-19 15:16:06 +0200

    add delete and update routes

--- routes/shop.js
+++ routes/shop.js
@@ -20,6 +20,14 @@ router.post('/category/create', category_controller.category_create_post);
 
 router.get('/category/:id', category_controller.category_read);
 
+router.get('/category/:id/update', category_controller.category_update_get);
+
+router.post('/category/:id/update', category_controller.category_update_post);
+
+router.get('/category/:id/delete', category_controller.category_delete_get);
+
+router.post('/category/:id/delete', category_controller.category_delete_post);
+
 // SubCategorys
 
 router.get('/subcategorys', subcategory_controller.subcategory_list);
@@ -29,6 +37,15 @@ router.get('/subcategory/create', subcategory_controller.subcategory_create_get)
 router.post('/subcategory/create', subcategory_controller.subcategory_create_post);
 
 router.get('/subcategory/:id', subcategory_controller.subcategory_read);
+
+router.get('/subcategory/:id/update', subcategory_controller.subcategory_update_get);
+
+router.post('/subcategory/:id/update', subcategory_controller.subcategory_update_post);
+
+router.get('/subcategory/:id/delete', subcategory_controller.subcategory_delete_get);
+
+router.post('/subcategory/:id/delete', subcategory_controller.subcategory_delete_post);
+
 // Products
 
 router.get('/products', product_controller.product_list);
@@ -38,5 +55,13 @@ router.get('/product/create', product_controller.product_create_get);
 router.post('/product/create', product_controller.product_create_post);
 
 router.get('/product/:id', product_controller.product_read);
+
+router.get('/product/:id/update', product_controller.product_update_get);
+
+router.post('/product/:id/update', product_controller.product_update_post);
+
+router.get('/product/:id/delete', product_controller.product_delete_get);
+
+router.post('/product/:id/delete', product_controller.product_delete_post);
 
 module.exports = router;

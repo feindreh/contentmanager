84ddf92558e5053941318426222a7381cb9b9a0f
feindreh <119032218+feindreh@users.noreply.github.com>
2023-04-19 13:23:41 +0200

    add read routes

--- routes/shop.js
+++ routes/shop.js
@@ -18,6 +18,8 @@ router.get('/category/create', category_controller.category_create_get);
 
 router.post('/category/create', category_controller.category_create_post);
 
+router.get('/category/:id', category_controller.category_read);
+
 // SubCategorys
 
 router.get('/subcategorys', subcategory_controller.subcategory_list);
@@ -25,10 +27,16 @@ router.get('/subcategorys', subcategory_controller.subcategory_list);
 router.get('/subcategory/create', subcategory_controller.subcategory_create_get);
 
 router.post('/subcategory/create', subcategory_controller.subcategory_create_post);
+
+router.get('/subcategory/:id', subcategory_controller.subcategory_read);
 // Products
 
 router.get('/products', product_controller.product_list);
 
 router.get('/product/create', product_controller.product_create_get);
+
+router.post('/product/create', product_controller.product_create_post);
+
+router.get('/product/:id', product_controller.product_read);
 
 module.exports = router;

571fb7801b0473a958761e28be019a4e298160bb
feindreh <119032218+feindreh@users.noreply.github.com>
2023-04-19 16:39:19 +0200

    add delete get + post

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -67,5 +67,41 @@ exports.category_read = async (req, res, next) => {
 //
 exports.category_update_get = async (req, res, next) => { res.send('IMplement me'); };
 exports.category_update_post = async (req, res, next) => { res.send('IMplement me'); };
-exports.category_delete_get = async (req, res, next) => { res.send('IMplement me'); };
-exports.category_delete_post = async (req, res, next) => { res.send('IMplement me'); };
+
+// display delete
+exports.category_delete_get = async (req, res, next) => {
+  try {
+    const [cat, subCats] = await Promise.all([
+      Category.findById(req.params.id),
+      SubCategory.find({ category: req.params.id }),
+    ]);
+
+    res.render('category_delete', {
+      cat,
+      subCats,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+// delete Category
+exports.category_delete_post = async (req, res, next) => {
+  try {
+    const [cat, subCats] = await Promise.all([
+      Category.findById(req.params.id),
+      SubCategory.find({ category: req.params.id }),
+    ]);
+    if (subCats.length > 0) {
+      res.render('category_delete', {
+        cat,
+        subCats,
+      });
+    } else {
+      await Category.findByIdAndRemove(req.params.id);
+      res.redirect('/shop/categorys');
+    }
+  } catch (err) {
+    next(err);
+  }
+};
